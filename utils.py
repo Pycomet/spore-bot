af249fb682cb1c3bda6db0458cc2c45b389cfbf7
Pycomet <alfredemmanuelinyang@gmail.com>
2022-12-21 10:47:46 +0100

    project setup

---
+++ utils.py
@@ -0,0 +1,76 @@
+from config import *
+from models import *
+
+
+def get_received_msg(msg):
+    "Delete This Message"
+    message_id = msg.message_id
+    chat = msg.chat
+    return chat, message_id
+
+
+def get_spreadsheet(name: str):
+    try:
+        service = build("sheets", "v4", credentials=creds)
+        spreadsheet = (
+            service.spreadsheets()
+            .values()
+            .get(
+                spreadsheetId=SPREADSHEET_ID, range=name
+            )
+            .execute()
+        )
+
+        logging.info("Google Sheet Connected! Fetch Complete")
+        data = spreadsheet["values"]
+        return data
+    except Exception as e:
+        logging.error("You do not have permission to access this spreadsheet")
+        return []
+
+
+class DbClient:
+    "For Reading, Updating & Deleting Spreadsheet Content"
+
+    def __init__(self):
+        self.stock_sheet = get_spreadsheet('STOCK SHEET')[1::]
+        self.order_sheet = get_spreadsheet('ORDER SHEET')[1::]
+
+    def get_users(self) -> list:
+        "Fetch All Users IDs in the sheet"
+        users = []
+
+        for item in self.stock_sheet:
+            user = User(
+                user_id=item[0], screen_name=item[1], user=item[2], agent=item[3]
+            )
+            users.append(user)
+        return users
+
+    def get_data(self) -> list:
+        "Fetch All Group Data from sheet"
+        data = []
+
+        for item in self.order_sheet:
+            raw = Data(
+                name= item[1],
+                club_id= int(item[2]),
+                agent= item[7],
+                agent_rb= int(item[8]),
+                ref_code= item[9],
+                group_id= item[10]
+            )
+            data.append(raw)
+        return data
+
+
+    def get_user(self) -> list:
+        "Fetch All Users"
+        pass
+
+    def user_exists(self) -> bool:
+        "If users exists in list or not"
+        pass
+
+
+db_client = DbClient()
